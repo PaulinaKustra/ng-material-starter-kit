35284f3b590585ff21646c433fc7298a7af42b67
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2022-11-27 14:24:41 +0100

    feat: list of products implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,3 +19,4 @@ import { HttpClientModule } from "@angular/common/http";
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
