7dcccaf8fae8e916f19dc92a2a199c925d6624c2
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2022-11-29 13:29:26 +0100

    feat: category selection implemented inside product form

--- src/app/components/product-form/product-form.component-module.ts
+++ src/app/components/product-form/product-form.component-module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { ProductFormComponent } from './product-form.component';
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, CommonModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, CommonModule, MatButtonModule, MatCardModule, MatListModule, MatSelectModule],
   declarations: [ProductFormComponent],
   providers: [],
   exports: [ProductFormComponent]
