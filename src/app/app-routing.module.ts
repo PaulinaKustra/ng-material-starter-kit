7d7a666c3c9e2ae7d7ac0757ad0f980a23497134
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2023-01-24 18:00:55 +0100

    cart details displayed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,23 @@ import { RouterModule } from '@angular/router';
 import { CatFactComponent } from './components/cat-fact/cat-fact.component';
 import { AgePredictionComponent } from './components/age-prediction/age-prediction.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CatFactComponentModule } from './components/cat-fact/cat-fact.component-module';
 import { CatFactServiceModule } from './services/cat-fact.service-module';
 import { AgePredictionComponentModule } from './components/age-prediction/age-prediction.component-module';
 import { AgePredictionServiceModule } from './services/age-prediction.service-module';
 import { ProductDetailsComponentModule } from './components/product-details/product-details.component-module';
 import { ProductDetailsServiceModule } from './services/product-details.service-module';
+import { CartDetailsComponentModule } from './components/cart-details/cart-details.component-module';
+import { CartServiceModule } from './services/cart.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
     { path: 'cat-fact', component: CatFactComponent },
     { path: 'age/:name', component: AgePredictionComponent },
-    { path: 'product/:id', component: ProductDetailsComponent }
-  ]), CatFactComponentModule, CatFactServiceModule, AgePredictionComponentModule, AgePredictionServiceModule, ProductDetailsComponentModule, ProductDetailsServiceModule],
+    { path: 'product/:id', component: ProductDetailsComponent },
+    { path: 'cart/:id', component: CartDetailsComponent }
+  ]), CatFactComponentModule, CatFactServiceModule, AgePredictionComponentModule, AgePredictionServiceModule, ProductDetailsComponentModule, ProductDetailsServiceModule, CartDetailsComponentModule, CartServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/models/cart.model.ts
@@ -0,0 +1,11 @@
+export interface CartModel {
+  id: number;
+  userId: number;
+  date: Date;
+  products: Product[];
+  __v: number;
+}
+export interface Product {
+  productId: number;
+  quantity: number;
+}
