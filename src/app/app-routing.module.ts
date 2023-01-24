3a007e91b89cbad66c9a2e524e8579f38d7fd9b7
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2023-01-24 18:15:06 +0100

    user details displayed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CatFactComponent } from './components/cat-fact/cat-fact.component';
 import { AgePredictionComponent } from './components/age-prediction/age-prediction.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { CatFactComponentModule } from './components/cat-fact/cat-fact.component-module';
 import { CatFactServiceModule } from './services/cat-fact.service-module';
 import { AgePredictionComponentModule } from './components/age-prediction/age-prediction.component-module';
@@ -12,14 +13,17 @@ import { ProductDetailsComponentModule } from './components/product-details/prod
 import { ProductDetailsServiceModule } from './services/product-details.service-module';
 import { CartDetailsComponentModule } from './components/cart-details/cart-details.component-module';
 import { CartServiceModule } from './services/cart.service-module';
+import { UserDetailsComponentModule } from './components/user-details/user-details.component-module';
+import { UserDetailsServiceModule } from './services/user-details.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
     { path: 'cat-fact', component: CatFactComponent },
     { path: 'age/:name', component: AgePredictionComponent },
     { path: 'product/:id', component: ProductDetailsComponent },
-    { path: 'cart/:id', component: CartDetailsComponent }
-  ]), CatFactComponentModule, CatFactServiceModule, AgePredictionComponentModule, AgePredictionServiceModule, ProductDetailsComponentModule, ProductDetailsServiceModule, CartDetailsComponentModule, CartServiceModule],
+    { path: 'cart/:id', component: CartDetailsComponent },
+    { path: 'user/:id', component: UserDetailsComponent }
+  ]), CatFactComponentModule, CatFactServiceModule, AgePredictionComponentModule, AgePredictionServiceModule, ProductDetailsComponentModule, ProductDetailsServiceModule, CartDetailsComponentModule, CartServiceModule, UserDetailsComponentModule, UserDetailsServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/services/user-details.service-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { UserDetailsService } from './user-details.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [UserDetailsService],
+  exports: []
+})
+export class UserDetailsServiceModule {
+}
