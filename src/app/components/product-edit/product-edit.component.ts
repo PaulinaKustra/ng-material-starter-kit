5366881bd7d702701d4455a195db4af11df8233e
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2023-01-30 11:35:02 +0100

    single product edit form updated

--- src/app/components/product-edit/product-edit.component.ts
+++ src/app/components/product-edit/product-edit.component.ts
@@ -1,7 +1,7 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation, ViewChild, ElementRef} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
-import {from, Observable, of} from 'rxjs';
+import {from, Observable, of, Subscription} from 'rxjs';
 import {switchMap, take, tap} from 'rxjs/operators';
 import {ProductDetailsModel} from '../../models/product-details.model';
 import {ProductDetailsService} from '../../services/product-details.service';
@@ -12,7 +12,7 @@ import {UploadClient} from "@uploadcare/upload-client";
   styleUrls: ['./product-edit.component.scss'],
   templateUrl: './product-edit.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class ProductEditComponent {
   readonly products$: Observable<ProductDetailsModel> = this._activatedRoute.params
@@ -26,44 +26,36 @@ export class ProductEditComponent {
     description: new FormControl(),
     category: new FormControl(),
     price: new FormControl(),
-    image: new FormControl(''),
-    avatar: new FormControl(),
+    image: new FormControl('')
   });
   readonly categories$: Observable<string[]> = this._productDetailsService.getAll();
 
   constructor(private _productDetailsService: ProductDetailsService, private _activatedRoute: ActivatedRoute) {
-    this.products$.subscribe();
+    this.subscription = this.products$.subscribe();
   }
 
   public imageURL: Observable<any> = of('test');
 
   public onFileSelected(event: any) {
-    console.log('onselect');
-
-    this.showPreview(event)
-  }
-
-  showPreview(event: any) {
     if (event && event.target && event.target.files[0]) {
       // @ts-ignore
       const file = (event.target as HTMLInputElement).files[0];
 
-      console.log('wczytano zdjęcie ', file);
-
       from(this.client.uploadFile(file)
       ).subscribe((result) => {
-        console.log('dane:', result)
+        // console.log('dane:', result)
         this.imageURL = of(result.cdnUrl);
         this.form.patchValue({image: result.cdnUrl})
-        console.log('this.imageUrl:', this.imageURL)
+        // console.log('this.imageUrl:', this.imageURL)
       })
     }
   }
 
+  subscription: Subscription | undefined;
   client = new UploadClient({publicKey: 'b66cb5aa61ed990132b2'})
 
   onFormSubmitted(form: FormGroup): void {
-    console.log('leci submit', form.value.image)
+    // console.log('leci submit', form.value.image)
     this._activatedRoute.params
       .pipe(
         take(1),
@@ -74,14 +66,19 @@ export class ProductEditComponent {
             ...form.value
           }))
       ).subscribe();
-
   }
+
 
   @ViewChild('fileUploader') fileUploader!: ElementRef;
 
   onRemoveButtonClicked() {
     this.fileUploader.nativeElement.value = null;
     this.imageURL = of('')
+    this.form.patchValue({image: '' })
   }
-
+  ngOnDestroy(): void {
+    if(this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
