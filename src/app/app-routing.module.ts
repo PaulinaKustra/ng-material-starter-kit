f2da6c4798ed8b2b5d5c26800fc1101520878b25
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2023-02-09 17:02:50 +0100

    filtering by categories added to the product list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductsComponentModule } from './components/products/products.component-module';
+import { CategoryServiceModule } from './services/category.service-module';
+import { ProductServiceModule } from './services/product.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([
+    { path: 'products', component: ProductsComponent }]),
+    ProductsComponentModule,
+    CategoryServiceModule,
+    ProductServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,45 @@
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {BehaviorSubject, combineLatest, map, Observable} from 'rxjs';
+import {ProductModel} from '../../models/product.model';
+import {CategoryService} from '../../services/category.service';
+import {ProductService} from '../../services/product.service';
+
+@Component({
+  selector: 'app-products',
+  styleUrls: ['./products.component.scss'],
+  templateUrl: './products.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductsComponent {
+  private _selectedCategorySubject: BehaviorSubject<string | undefined> =
+    new BehaviorSubject<string | undefined>(undefined);
+
+  public selectedCategory$: Observable<string | undefined> =
+    this._selectedCategorySubject.asObservable();
+
+  readonly categories$: Observable<string[]> =
+    this._categoryService.getAll();
+
+  readonly products$: Observable<ProductModel[]> =
+    combineLatest([
+      this._productService.getAll(),
+      this.selectedCategory$]).pipe(
+      map(([list, selectedCategory]) => {
+        if (!selectedCategory) {
+          return list;
+        }
+        return list.filter((product) => product.category === selectedCategory);
+      })
+    );
+
+
+  constructor(
+    private _categoryService: CategoryService,
+    private _productService: ProductService) {
+  }
+
+  onFilteringChanged(category: string): void {
+    this._selectedCategorySubject.next(category);
+  }
+}
